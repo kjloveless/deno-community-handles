dbd32f52dabe7aca46357818fd138bed0a93a8a2
Kyle Loveless <kyle@loveless.dev>
2024-10-06 16:19:59 -0500

    add clsx package and twind-merge

--- components/stage.tsx
+++ components/stage.tsx
@@ -1,5 +1,7 @@
 import { ComponentChildren } from "preact";
 
+import { cn } from "../util/mod.ts";
+
 interface HTMLStageElement {
   number: number;
   title: string;
@@ -12,7 +14,7 @@ export function Stage(
   { number, title, disabled, last, children }: HTMLStageElement,
 ) {
   return (
-    <section className={"opacity-50"}>
+    <section className={cn(disabled && "opacity-50")}>
       <div className="flex h-8 flex-row items-center">
         <div className="mr-4 grid size-8 shrink-0 place-items-center rounded-full 
                   bg-slate-100 text-center dark:bg-slate-800">
@@ -21,7 +23,10 @@ export function Stage(
         <h2 className="font-semibold">{title}</h2>
       </div>
       <div
-        className={"border-l-1 ml-4 border-l py-6 pl-8 border-transparent"}
+        className={cn(
+          "border-l-1 ml-4 border-l py-6 pl-8",
+          last && "border-transparent",
+        )}
       >
         {children}
       </div>
