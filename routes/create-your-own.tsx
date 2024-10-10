2884811cee7f431f0ef8f60a4ad1881550152e0e
Kyle Loveless <kyle@loveless.dev>
2024-10-10 13:08:45 -0500

    formatting, typo

--- routes/create-your-own.tsx
+++ routes/create-your-own.tsx
@@ -8,6 +8,7 @@ export default function CommunityPage() {
         <title>Create a Community Handle for your community</title>
         <meta name="description" content="Host your own tool" />
       </Head>
+
       <main className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
         <div className="flex max-w-[980px] flex-col items-start gap-4">
           <h1 className="text-3xl font-extrabold leading-tight tracking-tighter sm:text-3xl md:text-5xl lg:text-6xl">
@@ -16,7 +17,7 @@ export default function CommunityPage() {
           </h1>
           <p className="max-w-[700px] text-lg text-muted-foreground sm:text-xl">
             Want a custom community handle for your community, like
-            @alex.swifties.social, @eeyore@sad.blue or @jane.kawaii.social?
+            @alex.swifties.social, @eeyore.sad.blue or @jane.kawaii.social?
             Follow these steps to get one.
           </p>
         </div>
