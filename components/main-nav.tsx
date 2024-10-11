16b2719ede632889793bcac2ff99071aa8317a27
Kyle Loveless <kyle@loveless.dev>
2024-10-11 13:49:46 -0500

    move main-nav.tsx to components

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -1,30 +1,21 @@
-import { IS_BROWSER } from "$fresh/runtime.ts";
-
 import { Link } from "../islands/link.tsx";
 
 import { NavItem } from "../types/nav.ts";
 
 import { cn } from "../util/mod.ts";
 
-// import { Icons } from "@/components/icons";
+import { Icons } from "../components/icons.tsx";
 
 interface MainNavProps {
   title: string;
   items?: NavItem[];
 }
 
-export function MainNav({ title, items }: MainNavProps) {
-  let prefix = "";
-  if (IS_BROWSER) {
-    const base = typeof window !== "undefined" ? globalThis.location.host : "";
-    const isLocalhost = base.includes("localhost");
-    prefix = isLocalhost ? "/deno.social" : "";
-  }
-
+export default function MainNav({ title, items }: MainNavProps) {
   return (
     <div className="flex gap-6 lg:gap-10">
-      <Link href={prefix + "/"} className="flex items-center space-x-2">
-        {/* <Icons.logo className="size-6" /> */}
+      <Link href={"/"} className="flex items-center space-x-2">
+        <Icons.logo className="size-6" />
         <span className="inline-block font-bold">{title}</span>
       </Link>
       {items?.length
@@ -35,7 +26,7 @@ export function MainNav({ title, items }: MainNavProps) {
                 item.href && (
                   <Link
                     key={index}
-                    href={prefix + item.href}
+                    href={item.href}
                     className={cn(
                       "flex items-center text-lg font-semibold text-muted-foreground sm:text-sm",
                       item.disabled && "cursor-not-allowed opacity-80",
