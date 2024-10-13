58fe26509837f46ee0e09a866aad154db4cd1d1a
Kyle Loveless <kyle@loveless.dev>
2024-10-13 17:28:15 -0500

    fix nav link for root domain

--- util/mod.ts
+++ util/mod.ts
@@ -7,6 +7,9 @@ export function cn(...inputs: ClassValue[]) {
 
 export function getDomain(hostname: string) {
   const domainParts = hostname.split(".");
+  if (domainParts.length <= 2) {
+    return { domain: hostname, subdomain: undefined };
+  }
 
   const domain = `${domainParts[domainParts.length - 1]}
   ${domainParts[domainParts.length]}`;
