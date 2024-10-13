91ca5e1c2b6cd228916647e398eb216fcff578bc
Kyle Loveless <kyle@loveless.dev>
2024-10-13 11:51:42 -0500

    remove unneeded class style

--- islands/site-header.tsx
+++ islands/site-header.tsx
@@ -60,7 +60,7 @@ export default function SiteHeader({ children }: Props) {
       </header>
       {showMenu && (
         <div
-          className="fixed top-16 z-30 w-full overflow-hidden border-b bg-background/80 transition-transform duration-500 md:hidden translate-y-1px"
+          className="fixed top-16 z-30 w-full overflow-hidden border-b bg-background/80 transition-transform duration-500 md:hidden"
           aria-hidden={!showMenu}
         >
           <div className="container flex h-full flex-col items-center justify-stretch px-4 pb-2">
